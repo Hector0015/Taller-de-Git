a68fb6c82a06411170f334d2ecdd2d3fb96dd6ed
HéctorF <alufog5923@ieselcaminas.org>
2024-01-11 11:56:24 +0000

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,6 @@
 <?php
 //Autor: Sergio Gómez <alufog5923@ieselcaminas.org>
-//El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
+?>
